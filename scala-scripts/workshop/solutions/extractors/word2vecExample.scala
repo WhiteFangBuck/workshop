e12503dac3e04e25097508ff09c7ca56f0bdbf75
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2017-05-02 11:14:42 -0700

    Create word2vecExample.scala

---
+++ scala-scripts/workshop/solutions/extractors/word2vecExample.scala
@@ -0,0 +1,30 @@
+import org.apache.spark.ml.feature.Word2Vec
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.sql.Row
+import org.apache.spark.ml.feature._
+
+{{
+    val sentenceData = spark.createDataFrame(Seq(
+      (0.0, "It was a bright cold day in April, and the clocks were striking thirteen."),
+      (0.0, "The sky above the port was the color of television, tuned to a dead channel."),
+      (1.0, "It was love at first sight.")
+    )).toDF("label", "sentence")
+
+// Input data: Each row is a bag of words from a sentence or document.
+val documentDF = spark.createDataFrame(Seq(
+  "Hi I heard about Spark".split(" "),
+  "I wish Java could use case classes".split(" "),
+  "Logistic regression models are neat".split(" ")
+).map(Tuple1.apply)).toDF("text")
+
+// Learn a mapping from words to Vectors.
+val word2Vec = new Word2Vec()
+  .setInputCol("sentence")
+  .setOutputCol("result")
+  .setVectorSize(3)
+  .setMinCount(0)
+val model = word2Vec.fit(sentenceData)
+
+val result = model.transform(documentDF)
+result.collect().foreach { case Row(text: Seq[_], features: Vector) =>
+  println(s"Text: [${text.mkString(", ")}] => \nVector: $features\n") }
