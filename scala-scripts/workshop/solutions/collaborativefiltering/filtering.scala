7cdceb0914cf274ba7e05598dc9e39e8b12028f4
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2017-05-02 00:47:39 -0700

    Create filtering.scala

---
+++ scala-scripts/workshop/solutions/collaborativefiltering/filtering.scala
@@ -0,0 +1,33 @@
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.recommendation.ALS
+
+case class Rating(userId: Int, movieId: Int, rating: Float, timestamp: Long)
+def parseRating(str: String): Rating = {
+  val fields = str.split("::")
+  assert(fields.size == 4)
+  Rating(fields(0).toInt, fields(1).toInt, fields(2).toFloat, fields(3).toLong)
+}
+
+val ratings = spark.read.textFile("data/mllib/als/sample_movielens_ratings.txt")
+  .map(parseRating)
+  .toDF().as[Rating]
+val Array(training, test) = ratings.randomSplit(Array(0.8, 0.2))
+
+// Build the recommendation model using ALS on the training data
+val als = new ALS()
+  .setMaxIter(5)
+  .setRegParam(0.01)
+  .setUserCol("userId")
+  .setItemCol("movieId")
+  .setRatingCol("rating")
+val model = als.fit(training)
+
+// Evaluate the model by computing the RMSE on the test data
+val predictions = model.transform(test)
+
+val evaluator = new RegressionEvaluator()
+  .setMetricName("rmse")
+  .setLabelCol("rating")
+  .setPredictionCol("prediction")
+val rmse = evaluator.evaluate(predictions)
+println(s"Root-mean-square error = $rmse")
