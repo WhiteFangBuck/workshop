008acafc4c8e2877ef53f39c1c22146507797314
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2017-05-03 22:57:40 -0700

    Update naiveBayesExample.scala

--- scala-scripts/workshop/solutions/classifier/naiveBayesExample.scala
+++ scala-scripts/workshop/solutions/classifier/naiveBayesExample.scala
@@ -1,18 +1,42 @@
 import org.apache.spark.ml.classification.NaiveBayes
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
-val rawText = spark.read.format("csv").option("delimiter", "\t").load("data/SMSSpamCollection.txt").toDF("id", "text")
+val rawText = spark.read.format("csv")
+  .option("delimiter", "\t")
+  .load("data/SMSSpamCollection.txt")
+  .toDF("id", "text")
 
-val tokenized = new RegexTokenizer().setInputCol("text").setOutputCol("words").setPattern("\\W").transform(rawText)
+val tokenized = new RegexTokenizer()
+  .setInputCol("text")
+  .setOutputCol("words")
+  .setPattern("\\W")
+  .transform(rawText)
 
-val indexed = new StringIndexer().setInputCol("id").setOutputCol("label").fit(tokenized).transform(tokenized)
+val indexed = new StringIndexer()
+  .setInputCol("id")
+  .setOutputCol("label")
+  .fit(tokenized)
+  .transform(tokenized)
 
-val hashed = new HashingTF().setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(1000).transform(indexed)
+val hashed = new HashingTF()
+  .setInputCol("words")
+  .setOutputCol("rawFeatures")
+  .setNumFeatures(1000)
+  .transform(indexed)
 
-val tfidf = new IDF().setInputCol("rawFeatures").setOutputCol("features").fit(hashed).transform(hashed)
+val tfidf = new IDF()
+  .setInputCol("rawFeatures")
+  .setOutputCol("features")
+  .fit(hashed)
+  .transform(hashed)
 
 val Array(train, test) = tfidf.randomSplit(Array(.8, .2), 102059L)
 
-val model = new NaiveBayes().fit(train)
-val metrics = new BinaryClassificationEvaluator().setLabelCol("label").setRawPredictionCol("prediction")
+val model = new NaiveBayes()
+  .fit(train)
+
+val metrics = new BinaryClassificationEvaluator()
+  .setLabelCol("label")
+  .setRawPredictionCol("prediction")
+
 metrics.evaluate(predictions)
