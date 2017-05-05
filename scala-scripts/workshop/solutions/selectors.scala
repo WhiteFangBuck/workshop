3b5a3c8a2acb47e11a4539dc221e2702f9ec2a46
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2017-05-04 22:10:31 -0700

    Create selectors.scala

---
+++ scala-scripts/workshop/solutions/selectors.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.feature.PCA
+
+val rows = spark.read.format("csv").option("delimiter", " ").load("data/pca.txt")
+rows.show
+
+val data = Array(
+        Vectors.sparse(5, Seq((1, 1.0), (3, 7.0))),
+        Vectors.dense(2.0, 0.0, 3.0, 4.0, 5.0),
+        Vectors.dense(4.0, 0.0, 0.0, 6.0, 7.0)
+      )
+      
+      val df = spark.createDataFrame(data.map(Tuple1.apply)).toDF("features")
+      
+      val pca = new PCA()
+  .setInputCol("features")
+  .setOutputCol("pcaFeatures")
+  .setK(3)
+  .fit(df)
+  
+  val result = pca.transform(df).select("pcaFeatures")
